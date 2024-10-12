37dd344e43363ea06ee4530ed1759108aa1d16c4
brewmaster12 <seanywg@gmail.com>
2024-10-12 14:23:47 +0100

    Complete first 4 steps

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
- 
+// Step 1
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+// Step 2
+let n1;
+let operator;
+let n2;
+
+// Step 3
+function operate(n1, operation, n2) {
+    switch (operation) {
+        case "add":
+            return add(n1, n2);
+        case "subtract":
+            return subtract(n1, n2);
+        case "multiply":
+            return multiply(n1, n2);
+        case "divide":
+            return divide(n1, n2);
+    }
+}
